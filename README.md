9399271c2bd4f610b1bb8aa12e0a0402893a1c02
Phillip Manwaring <phillip.manwaring@gmail.com>
2020-08-04 23:08:52 -0400

    add logic for creating svg from count

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# serverless-readme-counter
+<p align="center">
+  <img height="150" src="https://d1wzvcwrgjaybe.cloudfront.net/repos/manwaring/readme-counter/readme-category-icon.png">
+  <img height="150" src="https://d1wzvcwrgjaybe.cloudfront.net/repos/manwaring/readme-counter/readme-repo-icon.png">
+</p>
+
+# Readme Counter
--- src/get-badge.ts
+++ src/get-badge.ts
@@ -11,12 +11,30 @@ export const handler = httpApi(
       const { organization, repository } = event.pathParameters;
       await recordVisit(organization, repository, event);
       const res = await RepoCounter.get({ organization, repository });
-      return success(res);
+      const badge = generateBadge(res.Item.count);
+      return success(badge);
     } catch (err) {
       return error(err);
     }
   }
 );
+
+function generateBadge(count: number): string {
+  const label = "Visit count";
+  const countLength = count.toString().length;
+  const labelWidth = 70;
+  const countWidth = 24 + 1 * countLength;
+  const width = labelWidth + countWidth;
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="20" version="1.1">\
+  <rect width="${labelWidth}" height="20" fill="#555"/>\
+  <rect x="${labelWidth}" width="${countWidth}" height="20" fill="#4C1"/>\
+  <rect rx="3" width="${width}" height="20" fill="transparent"/>\
+  <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">\
+  <text x="${labelWidth / 2}" y="14">${label}</text>\
+  <text x="${labelWidth + countWidth / 2}" y="14">${count}</text>\
+  </g>\
+  </svg>`;
+}
 
 function recordVisit(organization, repository, event): Promise<any> {
   const visit = {
